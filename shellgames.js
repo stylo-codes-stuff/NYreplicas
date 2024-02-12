11eb8f585c1cf245e6eec2a07f9b95206bbe2caf
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-02-12 04:50:11 +0000

    added wordle prototype

---
+++ shellgames.js
@@ -0,0 +1,5 @@
+/*all the shellgames but in js rather then python*/
+const array1 = [1,2,3,4,5]
+const array2 = [3,4,5]
+const filteredArray = array1.filter(value => array2.includes(value));
+console.log(filteredArray)
