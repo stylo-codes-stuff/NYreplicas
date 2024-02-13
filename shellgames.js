0c7f821a28efca981c0a459c8e687c2cdefe0b01
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-02-13 19:17:43 +0000

    changed the wordle shellgame to account for duplicate misplaced letters now if you guess a word that has one of a letter and in your guess that letter is misplaced twice it will only show you that you misplaced it the first time.

--- shellgames.js
+++ shellgames.js
@@ -1,5 +1,25 @@
 /*all the shellgames but in js rather then python*/
-const array1 = [1,2,3,4,5]
+const word = ["D","E","I","T","Y"]
+const guess = ["D","E","I","Y","T"]
+const array1 = [1,2,3]
 const array2 = [3,4,5]
-const filteredArray = array1.filter(value => array2.includes(value));
-console.log(filteredArray)
+let good_letters = []
+function correct_letters(guess,word){
+    let i = 0;
+        while (i < guess.length) {
+            if (guess[i] == word[i]){
+                good_letters.push(guess[i])
+            }
+            i++
+        }
+        return good_letters
+}
+function arrayIntersect (array1,array2){
+    return array1.filter(value => array2.includes(value));
+}
+function arrayDiff(array1,array2){
+    return array1.filter((element) => !array2.includes(element));
+
+}
+const text = "test"
+console.log(text.split(""));
